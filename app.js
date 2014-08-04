a479a6d2d72c6034c2a78bcea57d1cc3b47d2f35
gbachik <gbachik@gmail.com>
2014-08-04 17:12:04 -0600

    fixed setup

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', indexController.index);
 
-var server = app.listen(8317, function() {
+var port = process.env.PORT || 1337;
+var server = app.listen(port, function() {
 	console.log('Express server listening on port ' + server.address().port);
 });
